52a1138e2176b312395f3799d87fc589458a2511
Evgeny Varlamov <varlamov-el@mail.ru>
2023-03-01 21:39:42 +0300

    Docker compose trouble

--- src/main/java/ru/kata/spring/boot_security/demo/util/UsersInit.java
+++ src/main/java/ru/kata/spring/boot_security/demo/util/UsersInit.java
@@ -9,26 +9,26 @@ import ru.kata.spring.boot_security.demo.service.UserService;
 import javax.annotation.PostConstruct;
 import java.util.Set;
 
-//@Component
+@Component
 public class UsersInit {
-//    private final UserService userService;
-//    private final RoleService roleService;
-//
-//    @Autowired
-//    public UsersInit(UserService userService, RoleService roleService) {
-//        this.userService = userService;
-//        this.roleService = roleService;
-//    }
-//
-//    @PostConstruct
-//    private void postConstruct() {
-//        Role roleAdmin = new Role("ROLE_ADMIN");
-//        Role roleUser = new Role("ROLE_USER");
-//        roleService.saveRole(roleAdmin);
-//        roleService.saveRole(roleUser);
-//        User admin = new User("admin", "admin", "male", 38, Set.of(roleAdmin));
-//        User user = new User("user", "user", "male", 28, Set.of(roleUser));
-//        userService.saveUser(admin);
-//        userService.saveUser(user);
-//    }
+    private final UserService userService;
+    private final RoleService roleService;
+
+    @Autowired
+    public UsersInit(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @PostConstruct
+    private void postConstruct() {
+        Role roleAdmin = new Role("ROLE_ADMIN");
+        Role roleUser = new Role("ROLE_USER");
+        roleService.saveRole(roleAdmin);
+        roleService.saveRole(roleUser);
+        User admin = new User("admin", "admin", "male", 38, Set.of(roleAdmin));
+        User user = new User("user", "user", "male", 28, Set.of(roleUser));
+        userService.saveUser(admin);
+        userService.saveUser(user);
+    }
 }
